ede04c353b92f765e7dcc5034877ff4f8ba8c695
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-07-18 11:31:59 -0300

    se arregla db

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      PUBLIC_TURSO_DATABASE_URL: process.env.PUBLIC_TURSO_DATABASE_URL,
-      PUBLIC_TURSO_AUTH_TOKEN: process.env.PUBLIC_TURSO_AUTH_TOKEN,
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    },
+  env: {
+    TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
+    TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
   }
   
   module.exports = {
--- utils/config.ts
+++ utils/config.ts
@@ -1,25 +1,29 @@
-// Importa las variables de entorno correctamente
-const tursoConnectionUrl = process.env.PUBLIC_TURSO_DATABASE_URL as string;
-const tursoAuthToken = process.env.PUBLIC_TURSO_AUTH_TOKEN as string;
-const googleClientId = process.env.GOOGLE_CLIENT_ID as string;
-const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET as string;
+// file: utils/config.ts
+import { config as dotenvConfig } from 'dotenv';
+import path from 'path';
 
-// Configuración de la aplicación
-export const config = {
-  tursoConnectionUrl,
-  tursoAuthToken,
-  googleClientId,
-  googleClientSecret,
-  // ... otras configuraciones si las necesitas
+// Cargar variables de entorno desde .env.local solo en el servidor
+if (typeof window === 'undefined') {
+  dotenvConfig({ path: path.resolve(process.cwd(), '.env.local') });
+}
+
+const getEnvVariable = (key: string): string => {
+  const value = process.env[key];
+  if (!value) {
+    if (typeof window !== 'undefined') {
+      console.error(`Error: ${key} no está definido`);
+    } else {
+      throw new Error(`${key} no está definido`);
+    }
+  }
+  return value || '';
 };
 
-// Validación de la configuración
-if (!tursoConnectionUrl || !tursoAuthToken) {
-  throw new Error('Las variables de entorno PUBLIC_TURSO_DATABASE_URL y PUBLIC_TURSO_AUTH_TOKEN deben estar definidas');
-}
-
-if (!googleClientId || !googleClientSecret) {
-  throw new Error('Las variables de entorno GOOGLE_CLIENT_ID y GOOGLE_CLIENT_SECRET deben estar definidas');
-}
+export const config = {
+  tursoConnectionUrl: getEnvVariable('TURSO_DATABASE_URL'),
+  tursoAuthToken: getEnvVariable('TURSO_AUTH_TOKEN'),
+  googleClientId: getEnvVariable('GOOGLE_CLIENT_ID'),
+  googleClientSecret: getEnvVariable('GOOGLE_CLIENT_SECRET'),
+};
 
 export default config;
